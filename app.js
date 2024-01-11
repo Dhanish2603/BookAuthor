f36caf022e5d2d693749a339533912aa56545270
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2024-01-11 18:52:45 +0530

    final backend commit

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const authorRoutes = require('./routes/authorRotues');
-const bookRoutes = require('./routes/bookRoutes')
+const authorRoutes = require("./routes/authorRotues");
+const bookRoutes = require("./routes/bookRoutes");
 
 // middleware
 app.use(express.json());
@@ -18,23 +18,21 @@ app.use(cookieParser());
 app.use(cors());
 
 // routes
-app.use('/', authorRoutes);
-app.use('/books', bookRoutes);
-
-
+app.use("/", authorRoutes);
+app.use("/books", bookRoutes);
 
 // dotnev
-const PORT = process.env.PORT;
-const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
+const MONGO_URL =
+  process.env.MONGO_URL ||
+  "mongodb+srv://madhvesh:madhvesh@devtemp.lz30rdg.mongodb.net/book_Api?retryWrites=true&w=majority";
 
 // Connect Mongodb
 mongoose
-  .connect(
-    MONGO_URL
-  )
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Connected MONGODB");
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log(`Listening on Port 5000`);
     });
   })
